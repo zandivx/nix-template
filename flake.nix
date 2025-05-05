3e0c63091a769ee90a7c0f720e78574c57806b3b
zandivx <80193776+zandivx@users.noreply.github.com>
2025-05-05 23:42:39 +0200

    A very first starting point for a nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+    description = "A very basic flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+        # or give it another NAME with `nix develop .#NAME` instead of the following line:
+        devShells.${system}.default =
+            pkgs.mkShell {
+                shellHook = ''
+                    echo "Hello World"
+                '';
+            };
+    };
+}
